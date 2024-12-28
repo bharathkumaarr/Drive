2a05285c776195c36dc1d03b84e06aa29e2983e9
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2024-12-29 02:20:23 +0530

    using express validators to check username, pw, email validation

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 router.get('/register', (req,res)=> {
     res.render('register');
 })
 
-router.post('/register', (req,res)=> {
-    console.log(req.body);
-    res.send('user-registerd')
+router.post('/register', 
+    body('email').trim().isEmail(),
+    body('password').trim().isLength({min:5}),
+    body('username').trim().isLength({min:3}),
+    (req,res)=> {
+
+        const errors = validationResult(req)
+
+        console.log(errors);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                messages: 'invalid data'
+            })
+        }
+
+
+        res.send(errors)
 })
 
 
