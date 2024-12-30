49b4d6ecc088dafd31ccde1679b3ba400722b3e8
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2024-12-30 23:58:07 +0530

    updates

---
+++ models/user.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true,
+        unique: true,
+        minlength: [3,'Username must be at least 3 characters long']
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true,
+        unique: true,
+        minlength: [3,'Email must be at least 3 characters long']
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: [8,'Password must be at least 8 characters long']
+    }
+
+})
+
+const user=mongoose.model('user',userSchema)
+
+module.exports = user;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const userModel = require('../models/user.model')
+const bcrypt = require('bcrypt');
 
 router.get('/register', (req,res)=> {
     res.render('register');
 })
 
+
 router.post('/register', 
     body('email').trim().isEmail(),
     body('password').trim().isLength({min:5}),
     body('username').trim().isLength({min:3}),
-    (req,res)=> {
+    async (req,res)=> {
 
         const errors = validationResult(req)
 
@@ -24,7 +27,15 @@ router.post('/register',
         }
 
 
-        res.send(errors)
+        const {email, username, password} = req.body;
+        
+        const newUser = await userModel.create({
+            email,
+            username,
+            password
+        })
+
+        res.json(newUser)
 })
 
 
